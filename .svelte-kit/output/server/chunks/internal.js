40842a95395679a345ed7b2b0a6f138fb8863a7c
JackH <jackhui322@gmail.com>
2025-05-20 19:18:21 +1000

    Add .nojekyll to include _app folder

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -1729,7 +1729,7 @@ const options = {
 		<div class="error">\r
 			<span class="status">` + status + '</span>\r\n			<div class="message">\r\n				<h1>' + message + "</h1>\r\n			</div>\r\n		</div>\r\n	</body>\r\n</html>\r\n"
   },
-  version_hash: "fev8u3"
+  version_hash: "1stpimz"
 };
 async function get_hooks() {
   let handle;
