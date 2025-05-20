d390fbcdbefebbccc6b88b9240d52dd2ffcaf8ee
JackH <jackhui322@gmail.com>
2025-05-20 18:44:09 +1000

    fixed build without cname

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DBmHWvXb.js","_app/immutable/chunks/Hy_XaCxE.js","_app/immutable/chunks/ddMSHIDS.js","_app/immutable/chunks/iGh912gE.js","_app/immutable/chunks/EZ5IySk8.js","_app/immutable/chunks/Jo3crg2N.js","_app/immutable/chunks/C04hYcw7.js","_app/immutable/chunks/CWSGiRlO.js"];
+export const imports = ["_app/immutable/nodes/1.voBSxV6U.js","_app/immutable/chunks/Hy_XaCxE.js","_app/immutable/chunks/ddMSHIDS.js","_app/immutable/chunks/iGh912gE.js","_app/immutable/chunks/EZ5IySk8.js","_app/immutable/chunks/Jo3crg2N.js","_app/immutable/chunks/CXEA4NqZ.js","_app/immutable/chunks/CWSGiRlO.js"];
 export const stylesheets = [];
 export const fonts = [];
